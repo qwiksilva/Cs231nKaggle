ef8dbd0a8d8c9bc3a8ca39f3f697db598f906893
Ryan <rdsilva@stanford.edu>
2016-03-11 12:25:49 -0800

    1

--- KaggleModel/model.py
+++ KaggleModel/model.py
@@ -26,7 +26,7 @@ def get_model():
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
-    model.add(BatchNormalization(axis=1))
+    #model.add(BatchNormalization(axis=1))
 
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
     model.add(Dropout(0.25))
@@ -38,7 +38,7 @@ def get_model():
 
     model.add(Convolution2D(96, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
-    model.add(BatchNormalization(axis=1))
+    #model.add(BatchNormalization(axis=1))
 
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
     model.add(Dropout(0.25))
