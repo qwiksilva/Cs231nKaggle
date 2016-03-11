c90f5520413a87d41d4028c5aea5a7cc65933e58
Ubuntu <ubuntu@ip-172-31-5-149.us-west-1.compute.internal>
2016-03-11 19:43:18 +0000

    test

--- KaggleModel/model.py
+++ KaggleModel/model.py
@@ -21,7 +21,7 @@ def get_model():
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
-    model.add(BatchNormalization(axis=1))
+    model.add(BatchNormalization(mode=1))
 
     model.add(Convolution2D(128, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
