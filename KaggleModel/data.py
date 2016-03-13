b183ce52340cfd4cd4d38e0111f5fe94c03c9fa2
Ryan <rdsilva@stanford.edu>
2016-03-13 01:10:27 -0800

    idc

--- KaggleModel/data.py
+++ KaggleModel/data.py
@@ -221,7 +221,7 @@ def write_train_npy():
 
     X = np.array(X_new, dtype=np.uint8)
     y = np.array(y)
-    study_metadata = np.array(study_metadata, dtype=np.float64)
+    study_metadata = np.array(metadata, dtype=np.float64)
     np.save('/data/preprocessed/X_train.npy', X)
     np.save('/data/preprocessed/y_train.npy', y)
     np.save('/data/preprocessed/metadata_train.npy', study_metadata)
