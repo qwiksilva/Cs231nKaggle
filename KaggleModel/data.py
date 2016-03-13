03a3dc5da56d20e02c8cbbb5c719bf64766c533f
Ryan <rdsilva@stanford.edu>
2016-03-12 20:37:47 -0800

    double slice thinkness

--- KaggleModel/data.py
+++ KaggleModel/data.py
@@ -90,7 +90,7 @@ def load_images(from_dir, verbose=True):
                 
                 image = dicom.read_file(image_path)
                 if not pixel_scale:
-                    pixel_scale = float(image.PixelSpacing[0])
+                    pixel_scale = float(image.PixelSpacing[0])*2.0
                 if not slice_thickness:
                     slice_thickness = float(image.SliceThickness)
                 image = image.pixel_array.astype(float)
