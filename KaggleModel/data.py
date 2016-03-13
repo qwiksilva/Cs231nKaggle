e3c1fa2f83aa113d7ec38a5f7e7e375fe714755c
Ubuntu <ubuntu@ip-172-31-5-149.us-west-1.compute.internal>
2016-03-13 04:42:07 +0000

    data

--- KaggleModel/data.py
+++ KaggleModel/data.py
@@ -190,9 +190,13 @@ def write_train_npy():
     print('-'*50)
     # study_ids, images, all_metadata = load_images('D:/Documents/CS231N/dataset/train')
     study_ids, images, all_metadata = load_images('/data/KaggleData/train')
-    pickle.dump(study_ids, open('study_ids.p', 'wb'))
-    pickle.dump(images, open('images.p', 'wb'))
-    pickle.dump(all_metadata, open('all_metadata.p', 'wb'))
+    try:
+        pickle.dump(study_ids, open('/data/preprocessed/study_ids.p', 'wb'))
+        pickle.dump(images, open('/data/preprocessed/images.p', 'wb'))
+        pickle.dump(all_metadata, open('/data/preprocessed/all_metadata.p', 'wb'))
+    except:
+	pass
+
     studies_to_results = map_studies_results()  # load the dictionary of studies to targets
     X = []
     y = []
